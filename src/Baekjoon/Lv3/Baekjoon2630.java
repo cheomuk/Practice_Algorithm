0967606913c1e0489ab8870b4a1762d27cef0962
cheomuk <pkw9815@naver.com>
2023-12-12 01:21:36 +0900

    Commit 231211

--- src/Baekjoon/Lv3/Baekjoon2630.java
+++ src/Baekjoon/Lv3/Baekjoon2630.java
@@ -3,22 +3,20 @@ package Baekjoon.Lv3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Baekjoon2630 {
 
     static int[][] graph;
-    static boolean[][] visited;
-    static int num, count;
+    static int num, countWhite, countBlue;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         num = Integer.parseInt(br.readLine());
 
         graph = new int[num][num];
-        visited = new boolean[num][num];
-        count = 0;
+
+        countWhite = 0;
+        countBlue = 0;
 
         for (int i = 0; i < num; i++) {
             String[] arr = br.readLine().split(" ");
@@ -27,29 +25,42 @@ public class Baekjoon2630 {
             }
         }
 
-        for (int i = 0; i < num; i++) {
-            for (int j = 0; j < num; j++) {
-                if (graph[i][j] == 1 && !visited[i][j]) {
-                    find((int) Math.pow(num, 2), j, i);
-                }
-            }
-        }
+        find(num, 0, 0);
 
+        System.out.println(countWhite);
+        System.out.println(countBlue);
         br.close();
     }
 
     private static void find(int size, int x, int y) {
-        if (x < size / 2 && y < size / 2) {
-            find(size / 2, x, y);
-        } else if (x < size / 2 && y >= size / 2) {
-            count += size * size / 4;
-            find(size / 2, x, y - size / 2);
-        } else if (x >= size / 2 && y < size / 2) {
-            count += (size * size / 4) * 2;
-            find(size / 2, x - size / 2, y);
-        } else {
-            count += (size * size / 4) * 3;
-            find(size / 2, x - size / 2, y - size / 2);
+
+        if (checkBoard(size, x, y)) {
+            if (graph[y][x] == 1) {
+                countBlue++;
+            } else {
+                countWhite++;
+            }
+
+            return;
         }
+
+        find(size / 2, x, y);
+        find(size / 2, x, y + size / 2);
+        find(size / 2, x + size / 2, y);
+        find(size / 2, x + size / 2, y + size / 2);
+    }
+
+    private static boolean checkBoard(int size, int x, int y) {
+        int color = graph[y][x];
+
+        for (int i = y; i < y + size; i++) {
+            for (int j = x; j < x + size; j++) {
+                if (graph[i][j] != color) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
