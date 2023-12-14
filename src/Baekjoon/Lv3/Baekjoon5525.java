c8532cef1e3aa560026b38f22a3619f067af2975
cheomuk <pkw9815@naver.com>
2023-12-15 01:18:48 +0900

    Commit 231215

--- src/Baekjoon/Lv3/Baekjoon5525.java
+++ src/Baekjoon/Lv3/Baekjoon5525.java
@@ -3,66 +3,38 @@ package Baekjoon.Lv3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Baekjoon5525 {
 
-    static int N, M;
-    static int count = 0;
-
-    public static void main(String[] args) throws IOException {     // 50% 성공 -> 최적화 필요
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
 
-        String[] arr = br.readLine().split("");
-        Queue<String> queue = new LinkedList<>(Arrays.asList(arr));
+        String str = br.readLine();
 
-        String sn = createSn();
-        String str = "";
-        String word = "I";
+        int count = 0;
+        int index = 0;
 
-        while (!queue.isEmpty()) {
-            if (queue.peek().equals(word) && str.equals("")) {
-                str += queue.poll();
-            } else if (!str.equals("")) {
-                if (!queue.peek().equals(word)) {
-                    String s = queue.poll();
-                    str += s;
-                    word = s;
-                } else if (queue.peek().equals("I")) {
-                    str = queue.poll();
-                } else {
-                    queue.poll();
-                    str = "";
-                    word = "I";
+        for (int i = 1; i < M - 1; i++) {
+            if (str.startsWith("O", i) && str.startsWith("I", i + 1)) {
+                index++;
+                if (index == N) {
+                    if (str.startsWith("I", i - (index * 2 - 1))) {
+                        count++;
+                    }
+
+                    index--;
                 }
-            } else {
-                queue.poll();
-            }
 
-            if (sn.equals(str)) {
-                count++;
-                str = str.substring(2);
+                i++;
+            } else {
+                index = 0;
             }
         }
 
         System.out.println(count);
         br.close();
-    }
-
-    private static String createSn() {
-        char[] chars = new char[N * 2 + 1];
-        chars[0] = 'I';
-
-        for (int i = 1; i < N + 1; i++) {
-            chars[i * 2 - 1] = 'O';
-            chars[i * 2] = 'I';
-        }
-
-        return new String(chars);
     }
 }
