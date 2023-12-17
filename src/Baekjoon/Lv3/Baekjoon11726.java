a9d27bf0eb5a587db5169d607cec850bc73ed5a0
cheomuk <pkw9815@naver.com>
2023-12-17 22:23:22 +0900

    Commit 231217

---
+++ src/Baekjoon/Lv3/Baekjoon11726.java
@@ -0,0 +1,31 @@
+package Baekjoon.Lv3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon11726 {
+
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        dp = new int[num + 1];
+
+        if (num == 1) {
+            dp[1] = 1;
+        } else {
+            dp[1] = 1;
+            dp[2] = 2;
+
+            for (int i = 3; i <= num; i++) {
+                dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+            }
+        }
+
+        System.out.println(dp[num] % 10007);
+        br.close();
+    }
+}
