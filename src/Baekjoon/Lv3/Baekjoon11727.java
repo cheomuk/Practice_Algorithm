c67486709c27415b65d1c83069b37277cddbc7ce
cheomuk <pkw9815@naver.com>
2023-12-20 14:06:29 +0900

    Commit 231220

--- src/Baekjoon/Lv3/Baekjoon11727.java
+++ src/Baekjoon/Lv3/Baekjoon11727.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Baekjoon11726 {
+public class Baekjoon11727 {
 
     static int[] dp;
 
@@ -18,10 +18,10 @@ public class Baekjoon11726 {
             dp[1] = 1;
         } else {
             dp[1] = 1;
-            dp[2] = 2;
+            dp[2] = 3;
 
             for (int i = 3; i <= num; i++) {
-                dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+                dp[i] = (dp[i - 1] + (dp[i - 2] * 2)) % 10007;
             }
         }
 
