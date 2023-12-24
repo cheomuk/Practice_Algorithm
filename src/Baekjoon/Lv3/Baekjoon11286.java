5e6d96ce069eb982485e800aba849338d169740e
cheomuk <pkw9815@naver.com>
2023-12-24 23:33:14 +0900

    Commit 231224

---
+++ src/Baekjoon/Lv3/Baekjoon11286.java
@@ -0,0 +1,18 @@
+package Baekjoon.Lv3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon11286 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+
+
+        System.out.print(sb);
+        br.close();
+    }
+}
