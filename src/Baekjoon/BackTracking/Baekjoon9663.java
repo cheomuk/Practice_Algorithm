4aa0b08e8350808d27e67480c77d3a9ea54861f9
cheomuk <pkw9815@naver.com>
2024-01-15 05:00:45 +0900

    Commit 240114

--- src/Baekjoon/BackTracking/Baekjoon9663.java
+++ src/Baekjoon/BackTracking/Baekjoon9663.java
@@ -7,8 +7,7 @@ public class Baekjoon9663 {
 
     static int N;
     static int count = 0;
-    static int[][] graph;
-    static boolean[][] visited;
+    static int[][] visited;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,10 +15,16 @@ public class Baekjoon9663 {
 
         N = Integer.parseInt(br.readLine());
 
-        graph = new int[N + 1][N + 1];
-        visited = new boolean[N + 1][N + 1];
+        visited = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                visited[i][j] = -1;
+            }
+        }
 
         dfs(0);
+        sb.append(count).append("\n");
 
         System.out.println(sb);
         br.close();
@@ -32,9 +37,50 @@ public class Baekjoon9663 {
         }
 
         for (int i = 0; i < N; i++) {
-            graph[depth] = i;
-            dfs(start, depth + 1);
-            start++;
+            if (visited[depth][i] == -1) {
+                visited[depth][i] = 0;
+                fill(i, depth, 1);
+
+                dfs(depth + 1);
+
+                fill(i, depth, -1);
+                visited[depth][i] = -1;
+            }
+        }
+    }
+
+    private static void fill(int x, int y, int value) {
+        for (int i = 0; i < N; i++) {
+
+            // 상하
+            if (i != y) {
+                visited[i][x] += value;
+            }
+
+            // 좌우
+            if (i != x) {
+                visited[y][i] += value;
+            }
+
+            // 대각선 - 4시 방향
+            if (y + i < N && x + i < N) {
+                visited[y + i][x + i] += value;
+            }
+
+            // 대각선 - 10시 방향
+            if (y - i >= 0 && x - i >= 0) {
+                visited[y - i][x - i] += value;
+            }
+
+            // 대각선 - 7시 방향
+            if (y + i < N && x - i >= 0) {
+                visited[y + i][x - i] += value;
+            }
+
+            // 대각선 - 2시 방향
+            if (y - i >= 0 && x + i < N) {
+                visited[y - i][x + i] += value;
+            }
         }
     }
 }
