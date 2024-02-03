fea3ac8bb9868aefc81b5e3f846612fb9344a2f9
cheomuk <pkw9815@naver.com>
2024-02-04 01:46:40 +0900

    Commit 240203

--- src/Baekjoon/Greedy/Baekjoon11501.java
+++ src/Baekjoon/Greedy/Baekjoon11501.java
@@ -5,12 +5,42 @@ import java.io.InputStreamReader;
 
 public class Baekjoon11501 {
 
+    static int[] stocks;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
+        int num = Integer.parseInt(br.readLine());
 
+        for (int i = 0; i < num; i++) {
+            int n = Integer.parseInt(br.readLine());
+            String[] arr = br.readLine().split(" ");
 
-        System.out.println();
+            stocks = new int[n];
+
+            for (int j = 0; j < n; j++) {
+                stocks[j] = Integer.parseInt(arr[j]);
+            }
+
+            sb.append(find(0)).append("\n");
+        }
+
+        System.out.println(sb);
         br.close();
+    }
+
+    private static long find(int max) {
+        long count = 0;
+
+        for (int i = stocks.length - 1; i >= 0; i--) {
+            if (max < stocks[i]) {
+                max = stocks[i];
+            } else {
+                count += max - stocks[i];
+            }
+        }
+
+        return count;
     }
 }
