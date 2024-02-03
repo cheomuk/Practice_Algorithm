42517b25ac15053a4b89e63750293836a99b2460
cheomuk <pkw9815@naver.com>
2024-02-03 22:39:36 +0900

    Commit 240203

---
+++ src/Baekjoon/Greedy/Baekjoon11501.java
@@ -0,0 +1,16 @@
+package Baekjoon.Greedy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Baekjoon11501 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+        System.out.println();
+        br.close();
+    }
+}
