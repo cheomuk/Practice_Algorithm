2dadc3e415888477d9debec1c30fa2114b4d62ba
cheomuk <pkw9815@naver.com>
2024-02-14 18:52:22 +0900

    Commit 240214

---
+++ src/Baekjoon/Lv2/Baekjoon7568.java
@@ -0,0 +1,43 @@
+package Baekjoon.Lv2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Baekjoon7568 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N][2];
+
+        for(int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            arr[i][0] = Integer.parseInt(input[0]);	// 몸무게
+            arr[i][1] = Integer.parseInt(input[1]);	// 키
+        }
+
+
+        for (int i = 0; i < N; i++) {
+            int rank = 1;
+
+            for (int j = 0; j < N; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    rank++;
+                }
+            }
+
+            sb.append(rank).append(" ");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
