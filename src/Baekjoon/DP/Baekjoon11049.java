7b1cea3456ecbd94d4c614f1c873fc52f6620882
cheomuk <pkw9815@naver.com>
2024-03-21 22:56:55 +0900

    Commit 240321

--- src/Baekjoon/DP/Baekjoon11049.java
+++ src/Baekjoon/DP/Baekjoon11049.java
@@ -12,30 +12,40 @@ public class Baekjoon11049 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int num = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        graph = new int[num][2];
-        dp = new int[500][500];
+        graph = new int[n][2];
+        dp = new int[n][n];
 
-        for (int i = 0; i < num; i++) {
-            String[] arr = br.readLine().split(" ");
-
-            graph[i][0] = Integer.parseInt(arr[0]);
-            graph[i][1] = Integer.parseInt(arr[1]);
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            graph[i][0] = Integer.parseInt(input[0]);
+            graph[i][1] = Integer.parseInt(input[1]);
         }
 
-        sb.append(find()).append("\n");
+        sb.append(find(0, n - 1)).append("\n");
 
         System.out.println(sb);
         br.close();
     }
 
-    private static int find() {
+    private static int find(int start, int end) {
+        if (start == end) {
+            return 0;
+        }
+
+        if (dp[start][end] != 0) {
+            return dp[start][end];
+        }
+
         int min = Integer.MAX_VALUE;
 
-        for (int i = 0; i < graph.length; i++) {
-            for ()
+        for (int i = start; i < end; i++) {
+            int cost = find(start, i) + find(i + 1, end) + graph[start][0] * graph[i][1] * graph[end][1];
+            min = Math.min(min, cost);
         }
+
+        dp[start][end] = min;
 
         return min;
     }
