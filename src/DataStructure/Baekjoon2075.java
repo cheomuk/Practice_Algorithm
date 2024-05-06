7266b251fad0616c147c067cc590b94db5a0f500
cheomuk <pkw9815@naver.com>
2024-05-06 11:30:31 +0900

    Commit 240506

---
+++ src/DataStructure/Baekjoon2075.java
@@ -0,0 +1,35 @@
+package DataStructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Baekjoon2075 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String[] arr = br.readLine().split(" ");
+            for (int j = 0; j < N; j++) {
+                pq.add(Integer.parseInt(arr[j]));
+            }
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            pq.remove();
+        }
+
+        sb.append(pq.poll());
+
+        System.out.println(sb);
+        br.close();
+    }
+}
