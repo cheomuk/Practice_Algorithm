e4cbd0e6b6cf1192677246825127c0d0b3b8ad9d
cheomuk <pkw9815@naver.com>
2024-05-14 21:06:15 +0900

    Commit 240514

--- src/Baekjoon/Math/Baekjoon1402.java
+++ src/Baekjoon/Math/Baekjoon1402.java
@@ -15,6 +15,7 @@ public class Baekjoon1402 {
         // 9 4
         // 9를 소인수 분해하면 3 x 3 -> 3 + 3 = 6 이므로 6으로 변환 가능
         // 2 + 3 = 5 -> 1 + 4 or 5 x 1 -> 4 로 변환 가능
+        // 즉, 1과 -1 을 사용하면 실수는 전부 구현 가능하므로 yes를 출력한다.
         for (int i = 0; i < T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
