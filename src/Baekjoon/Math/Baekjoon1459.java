a716c9a12a550405ecf12432b8511598f5e0aab6
cheomuk <pkw9815@naver.com>
2024-05-31 18:50:32 +0900

    Commit 240531

---
+++ src/Baekjoon/Math/Baekjoon1459.java
@@ -0,0 +1,34 @@
+package Baekjoon.Math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon1459 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long x = Long.parseLong(st.nextToken());
+        long y = Long.parseLong(st.nextToken());
+        long w = Long.parseLong(st.nextToken());
+        long s = Long.parseLong(st.nextToken());
+
+        long temp1, temp2, temp3;
+
+        temp1 = (x+y) * w;
+
+        if ((x + y) % 2 == 0){
+            temp2 = Math.max(x, y) * s;
+        } else {
+            temp2 = (Math.max(x, y) - 1) * s + w;
+        }
+
+        temp3 = (Math.min(x, y)) * s + (Math.abs(x - y)) * w;
+
+        System.out.println(Math.min(temp1, Math.min(temp2, temp3)));
+        br.close();
+    }
+}
