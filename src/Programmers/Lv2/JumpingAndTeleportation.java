587c34ac94796ba5e0a008602a0c3aaae8164c34
cheomuk <pkw9815@naver.com>
2024-06-24 19:11:44 +0900

    Commit 240624

---
+++ src/Programmers/Lv2/JumpingAndTeleportation.java
@@ -0,0 +1,21 @@
+package Programmers.Lv2;
+
+public class JumpingAndTeleportation {
+
+    public int solution(int n) {
+        int ans = 0;
+
+        while (n > 0) {
+            if (n % 2 == 0) {
+                // 순간 이동
+                n /= 2;
+            } else {
+                // 점프
+                n -= 1;
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
