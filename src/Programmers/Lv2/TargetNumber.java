8c10a33ecbfbdc52c627a7cd6b74e4d540ebbc72
cheomuk <pkw9815@naver.com>
2024-07-08 06:31:46 +0900

    Commit 240708

--- src/Programmers/Lv2/TargetNumber.java
+++ src/Programmers/Lv2/TargetNumber.java
@@ -1,2 +1,16 @@
-package Programmers.Lv2;public class TargetNumber {
+package Programmers.Lv2;
+
+public class TargetNumber {
+    public int solution(int[] numbers, int target) {
+        return dfs(numbers, 0, 0, target);
+    }
+
+    private int dfs(int[] numbers, int index, int sum, int target) {
+        if (index == numbers.length) {
+            return sum == target ? 1 : 0;
+        }
+
+        return dfs(numbers, index + 1, sum + numbers[index], target)
+                + dfs(numbers, index + 1, sum - numbers[index], target);
+    }
 }
